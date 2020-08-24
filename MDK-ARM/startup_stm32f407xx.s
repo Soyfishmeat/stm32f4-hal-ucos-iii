0b0ddb365a116e137fc4974b78b5203b8a5dffa2
xyj <xiangyongjie@uxdar.com>
2020-08-24 17:18:56 +0800

    enable the FPU

--- MDK-ARM/startup_stm32f407xx.s
+++ MDK-ARM/startup_stm32f407xx.s
@@ -171,6 +171,23 @@ Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
         IMPORT  SystemInit
         IMPORT  __main
+                IF {FPU} != "SoftVFP"
+                                                ; Enable Floating Point Support at reset for FPU
+                LDR.W   R0, =0xE000ED88         ; Load address of CPACR register
+                LDR     R1, [R0]                ; Read value at CPACR
+                ORR     R1, R1, #(0xF <<20)     ; Set bits 20-30 to enable CP10 and CP11 coprocessors
+                                                ; Write back the modified CPACR value
+                STR     R1, [R0]                ; Wait for store to complete
+                DSB
+                
+                                                ; Disable automatic FP register content
+                                                ; Disable lazy context switch
+                LDR.W   R0, =0xE000EF34         ; Load address to FPCCR register
+                LDR     R1, [R0]
+                AND     R1, R1, #(0x3FFFFFFF)   ; Clear the LSPEN and ASPEN bits
+                STR     R1, [R0]
+                ISB                             ; Reset pipeline now the FPU is enable
+                ENDIF
 
                  LDR     R0, =SystemInit
                  BLX     R0
